238026f7abf8e7d97fb2df33ad17009c65693272
kewoodh <kesean.woodhouse@gmail.com>
2017-09-06 16:24:56 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const keyPresses = []
 
-function init() {
-  // Write your JavaScript code inside the init() function
+  function init() {
+    document.body.addEventListener('keydown', (e) => {
+      console.log(e.keyCode)
+      keyPresses.push(e.keyCode)
+      komaniCheck()
+    })
+  }
 
-}
+  function komaniCheck(){
+    if (keyPresses.length > 10) {
+      keyPresses.shift()
+    }
+    if (keyPresses.join('') === code.join('')) {
+      window.alert("Konami Code!")
+    }
+  }
