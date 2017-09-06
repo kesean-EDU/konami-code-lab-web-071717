894ea3987695861e541fa09c83b90cdf952fb985
kewoodh <kesean.woodhouse@gmail.com>
2017-09-06 16:27:21 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,6 +14,6 @@ const keyPresses = []
       keyPresses.shift()
     }
     if (keyPresses.join('') === code.join('')) {
-      window.alert("Konami Code!")
+      alert("Konami Code!")
     }
   }
